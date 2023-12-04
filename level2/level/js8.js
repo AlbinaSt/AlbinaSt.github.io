616a8faa7a40b7c74d98e387293db80f0b39fb76
AlbinaSt <144252565+AlbinaSt@users.noreply.github.com>
2023-12-04 19:59:02 +0300

    Update js8.js

--- level2/level/js8.js
+++ level2/level/js8.js
@@ -1,40 +1,31 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const openFormBtn = document.getElementById("openFormBtn");
-  const closeFormBtn = document.getElementById("closeForm");
-  const feedbackForm = document.getElementById("feedbackForm");
-  const contactForm = document.getElementById("contactForm");
-  const responseMessage = document.getElementById("responseMessage");
-
-  openFormBtn.addEventListener("click", function() {
-    feedbackForm.style.display = "block";
-  });
-
-  closeFormBtn.addEventListener("click", function() {
-    feedbackForm.style.display = "none";
-    responseMessage.innerHTML = "";
-  });
-
-  contactForm.addEventListener("submit", function(event) {
-    event.preventDefault();
-    const formData = new FormData(contactForm);
-
-    fetch("https://formcarry.com/s/jueViKGUnz", {
-      method: "POST",
-      body: formData
-    })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error("Ошибка отправки формы");
-      }
-      return response.json();
-    })
-    .then(data => {
-      responseMessage.innerHTML = "Форма успешно отправлена!";
-      contactForm.reset();
-    })
-    .catch(error => {
-      responseMessage.innerHTML = "Произошла ошибка при отправке формы.";
-      console.error(error.message);
+    // Показать попап при клике на кнопку
+    document.getElementById('openPopup').addEventListener('click', function() {
+      document.getElementById('popup').style.display = 'block';
     });
-  });
-});
+
+    // Обработка отправки формы
+    document.getElementById('myForm').addEventListener('submit', function(event) {
+      event.preventDefault();
+      const formData = new FormData(this);
+
+      fetch('https://formcarry.com/s/ВАШ_ID_ФОРМЫ', {
+        method: 'POST',
+        body: formData
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Ошибка при отправке данных');
+        }
+        return response.json();
+      })
+      .then(data => {
+        document.getElementById('message').innerText = 'Данные успешно отправлены';
+        // Дополнительные действия после успешной отправки
+        console.log(data);
+      })
+      .catch(error => {
+        document.getElementById('message').innerText = error.message || 'Что-то пошло не так';
+        // Дополнительные действия при ошибке
+        console.error(error);
+      });
+    });
